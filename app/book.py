406df1f6f4ea295961fc38e2bc21acb7e270ca82
Yannick Gicquel <ygicquel@gmail.com>
2021-11-14 18:04:01 +0100

    book: avoid extra json/dict conversions
    Signed-off-by: Yannick Gicquel <ygicquel@gmail.com>

--- app/book.py
+++ app/book.py
@@ -71,7 +71,7 @@ class Book:
             "tags": self.tags,
             "bookmarked": self.bookmarked,
         }
-        return json.dumps(d, default=lambda o: o.__dict__, indent=2)
+        return d
 
 class BookDir:
 
@@ -115,14 +115,14 @@ class BookDir:
     def to_json(self):
         d = {
             'dirpath': self.dirpath,
-            # Following reconstruct a dict from b.to_json() string. XXX
-            'booklist': [ json.loads(b.to_json()) for b in self.booklist ],
+            'booklist': [ b.to_json() for b in self.booklist ],
         }
-        return json.dumps(d, default=lambda o: o.__dict__, indent=2)
+        return d
 
     def save_db(self):
         with open(self.dbfile, "w") as f:
-            s = self.to_json()
+            d = self.to_json()
+            s = json.dumps(d, default=lambda o: o.__dict__, indent=2)
             f.write(s)
 
     def find_book_by_sha1(self, sha1):
