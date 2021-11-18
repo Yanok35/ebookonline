18058a4baed32bae89009cc5deb70e50f8614c2a
Yannick Gicquel <ygicquel@gmail.com>
2021-11-18 20:28:33 +0100

    all: add licence file and copyright notice
    Signed-off-by: Yannick Gicquel <ygicquel@gmail.com>

--- config.py
+++ config.py
@@ -1,5 +1,22 @@
 # -*- coding:utf-8 -*-
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+
+# eBookOnline - A web portal to access your ebooks collection
+# Copyright (C) 2021 Yannick Gicquel
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from __future__ import unicode_literals
 
 import os
